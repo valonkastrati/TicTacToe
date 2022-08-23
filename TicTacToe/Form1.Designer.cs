7c496d93c47b127fe383f99da25708aeb46c2a97
valonkastrati <valonkastrati@gmail.com>
2022-08-23 09:36:43 +0200

    Add project files.

---
+++ TicTacToe/Form1.Designer.cs
@@ -0,0 +1,288 @@
+﻿namespace TicTacToe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnFilloLojen = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblRadha = new System.Windows.Forms.Label();
+            this.panelLoja = new System.Windows.Forms.Panel();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.labelFituesi = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.checkKunderKompj = new System.Windows.Forms.CheckBox();
+            this.checkKunderKompjX = new System.Windows.Forms.CheckBox();
+            this.panelLoja.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(11, 231);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(175, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Radhen e ka:";
+            // 
+            // btnFilloLojen
+            // 
+            this.btnFilloLojen.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFilloLojen.Location = new System.Drawing.Point(17, 128);
+            this.btnFilloLojen.Margin = new System.Windows.Forms.Padding(2);
+            this.btnFilloLojen.Name = "btnFilloLojen";
+            this.btnFilloLojen.Size = new System.Drawing.Size(170, 44);
+            this.btnFilloLojen.TabIndex = 1;
+            this.btnFilloLojen.Text = "Fillo lojen";
+            this.btnFilloLojen.UseVisualStyleBackColor = true;
+            this.btnFilloLojen.Click += new System.EventHandler(this.btnFilloLojen_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 56F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(14, 16);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 87);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "_";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.UKlikuaNjeButon);
+            // 
+            // lblRadha
+            // 
+            this.lblRadha.AutoSize = true;
+            this.lblRadha.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRadha.Location = new System.Drawing.Point(178, 231);
+            this.lblRadha.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblRadha.Name = "lblRadha";
+            this.lblRadha.Size = new System.Drawing.Size(29, 31);
+            this.lblRadha.TabIndex = 3;
+            this.lblRadha.Text = "_";
+            // 
+            // panelLoja
+            // 
+            this.panelLoja.Controls.Add(this.button9);
+            this.panelLoja.Controls.Add(this.button8);
+            this.panelLoja.Controls.Add(this.button7);
+            this.panelLoja.Controls.Add(this.button4);
+            this.panelLoja.Controls.Add(this.button5);
+            this.panelLoja.Controls.Add(this.button6);
+            this.panelLoja.Controls.Add(this.button3);
+            this.panelLoja.Controls.Add(this.button2);
+            this.panelLoja.Controls.Add(this.button1);
+            this.panelLoja.Enabled = false;
+            this.panelLoja.Location = new System.Drawing.Point(313, 20);
+            this.panelLoja.Margin = new System.Windows.Forms.Padding(2);
+            this.panelLoja.Name = "panelLoja";
+            this.panelLoja.Size = new System.Drawing.Size(319, 300);
+            this.panelLoja.TabIndex = 4;
+            // 
+            // button9
+            // 
+            this.button9.Font = new System.Drawing.Font("Microsoft Sans Serif", 56F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button9.Location = new System.Drawing.Point(212, 200);
+            this.button9.Margin = new System.Windows.Forms.Padding(2);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(94, 87);
+            this.button9.TabIndex = 17;
+            this.button9.Text = "_";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.UKlikuaNjeButon);
+            // 
+            // button8
+            // 
+            this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 56F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button8.Location = new System.Drawing.Point(112, 200);
+            this.button8.Margin = new System.Windows.Forms.Padding(2);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(94, 87);
+            this.button8.TabIndex = 16;
+            this.button8.Text = "_";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.UKlikuaNjeButon);
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 56F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.Location = new System.Drawing.Point(14, 200);
+            this.button7.Margin = new System.Windows.Forms.Padding(2);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(94, 87);
+            this.button7.TabIndex = 15;
+            this.button7.Text = "_";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.UKlikuaNjeButon);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 56F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(14, 108);
+            this.button4.Margin = new System.Windows.Forms.Padding(2);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(94, 87);
+            this.button4.TabIndex = 14;
+            this.button4.Text = "_";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.UKlikuaNjeButon);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 56F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(112, 108);
+            this.button5.Margin = new System.Windows.Forms.Padding(2);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(94, 87);
+            this.button5.TabIndex = 13;
+            this.button5.Text = "_";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.UKlikuaNjeButon);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 56F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(212, 108);
+            this.button6.Margin = new System.Windows.Forms.Padding(2);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(94, 87);
+            this.button6.TabIndex = 12;
+            this.button6.Text = "_";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.UKlikuaNjeButon);
+            // 
+            // labelFituesi
+            // 
+            this.labelFituesi.AutoSize = true;
+            this.labelFituesi.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelFituesi.Location = new System.Drawing.Point(321, 326);
+            this.labelFituesi.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelFituesi.Name = "labelFituesi";
+            this.labelFituesi.Size = new System.Drawing.Size(104, 31);
+            this.labelFituesi.TabIndex = 11;
+            this.labelFituesi.Text = "______";
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 56F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(212, 16);
+            this.button3.Margin = new System.Windows.Forms.Padding(2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(94, 87);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "_";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.UKlikuaNjeButon);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 56F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(112, 16);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 87);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "_";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.UKlikuaNjeButon);
+            // 
+            // checkKunderKompj
+            // 
+            this.checkKunderKompj.AutoSize = true;
+            this.checkKunderKompj.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkKunderKompj.Location = new System.Drawing.Point(17, 36);
+            this.checkKunderKompj.Margin = new System.Windows.Forms.Padding(2);
+            this.checkKunderKompj.Name = "checkKunderKompj";
+            this.checkKunderKompj.Size = new System.Drawing.Size(213, 30);
+            this.checkKunderKompj.TabIndex = 5;
+            this.checkKunderKompj.Text = "0 eshte Kompjuteri";
+            this.checkKunderKompj.UseVisualStyleBackColor = true;
+            this.checkKunderKompj.CheckedChanged += new System.EventHandler(this.checkKunderKompj_CheckedChanged);
+            // 
+            // checkKunderKompjX
+            // 
+            this.checkKunderKompjX.AutoSize = true;
+            this.checkKunderKompjX.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkKunderKompjX.Location = new System.Drawing.Point(17, 80);
+            this.checkKunderKompjX.Margin = new System.Windows.Forms.Padding(2);
+            this.checkKunderKompjX.Name = "checkKunderKompjX";
+            this.checkKunderKompjX.Size = new System.Drawing.Size(216, 30);
+            this.checkKunderKompjX.TabIndex = 6;
+            this.checkKunderKompjX.Text = "X eshte Kompjuteri";
+            this.checkKunderKompjX.UseVisualStyleBackColor = true;
+            this.checkKunderKompjX.CheckedChanged += new System.EventHandler(this.checkKunderKompjX_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(642, 366);
+            this.Controls.Add(this.checkKunderKompjX);
+            this.Controls.Add(this.checkKunderKompj);
+            this.Controls.Add(this.panelLoja);
+            this.Controls.Add(this.lblRadha);
+            this.Controls.Add(this.btnFilloLojen);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelFituesi);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "TicTacToe";
+            this.panelLoja.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnFilloLojen;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblRadha;
+        private System.Windows.Forms.Panel panelLoja;
+        private System.Windows.Forms.Label labelFituesi;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.CheckBox checkKunderKompj;
+        private System.Windows.Forms.CheckBox checkKunderKompjX;
+    }
+}
+
---
+++ TicTacToe/Form1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// objekti qe gjeneron numra te rastesishem
+        /// </summary>
+        Random rasti = new Random();
+        
+        /// <summary>
+        /// numeruesi i levizjeve, per te ditur kur mbaron loja
+        /// </summary>
+        int levizje = 0;
+
+
+        /// <summary>
+        /// kjo metode inicon variablet ne fillim te lojes
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnFilloLojen_Click(object sender, EventArgs e)
+        {
+            // ketu fillon loja
+            levizje = 0;
+            foreach (Control b in panelLoja.Controls)
+            {
+                Button b1 = (b as Button);
+                b1.Text = "";
+            }
+            // paneli me butona behet enabled
+            panelLoja.Enabled = true;
+
+            //caktohet kush e ka radhen (fillon 0 gjithmone)
+            lblRadha.Text = "0";
+
+            labelFituesi.Text = "Fitues: ???";
+
+            // nese fillon kompjuteri
+            if (checkKunderKompj.Checked)
+                LuajVete();
+        }
+
+        /// <summary>
+        /// Kjo metode thirret ne Click te seciles fushe te lojes
+        /// </summary>
+        /// <param name="sender">butoni qe eshte klikuar</param>
+        /// <param name="e"></param>
+        private void UKlikuaNjeButon(object sender, EventArgs e)
+        {
+
+            //se pari e gjejme butonin qe u klikua
+            var b = sender as Button;
+
+            //nese ka qene i klikuar me heret, mos bej asgje
+            if (b.Text != "")
+                return;
+
+            
+            levizje++;
+
+            //vendose ne buton 0 ose X, varesisht kush e ka pasur radhen
+            b.Text = lblRadha.Text;
+
+            //duhet te shohim mos ka fitues
+            string pergjigja = ShikoAKaFitues();
+
+            if (pergjigja != "")
+            {
+                // nese Po, shfaqe mesazhin
+                labelFituesi.Text = pergjigja;
+
+                // beje disable panelin e lojes
+                panelLoja.Enabled = false;
+                return;
+            }
+
+            // perndryshe, vazhdon loja
+            // nderrohet radha (nga X ne 0 ose nga 0 ne X)
+            if (lblRadha.Text == "0")
+                lblRadha.Text = "X";
+            else
+                lblRadha.Text = "0";
+
+
+            //nese luajme kunder kompjuterit dhe ai e ka radhen
+            if (checkKunderKompj.Checked && lblRadha.Text == "0")
+                LuajVete();
+
+            if (checkKunderKompjX.Checked && lblRadha.Text == "X")
+                LuajVete();
+
+        }
+
+        /// <summary>
+        /// Kjo metode thirret per te bere levizje te rastesishme 
+        /// kur luajme kunder kompjuterit
+        /// </summary>
+        private void LuajVete()
+        {
+            // duhet te tentojme disa here 
+            // derisa te "zgjedhim" ne menyre te rastesishme
+            // nje fushe te lire
+            while (true)
+            {
+                int numri = rasti.Next(1, 10); //1-9
+                string emri = "button" + numri;
+                var ctl = panelLoja.Controls[emri];
+                // nese eshte gjetur nje fushe e lire aplikohet levizja
+                if (ctl.Text == "")
+                {
+                    // bejme nje refresh 
+                    Text = "Duke menduar...";
+                    this.Refresh();
+                    // bejme sikur kompjuteri ka menduar 1 sekonde
+                    Thread.Sleep(1000);
+                    Text = "Tic Tac Toe";
+
+                    // kompjuteri gjoja e realizon levizjen
+                    UKlikuaNjeButon(ctl, null);
+                    return;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Kjo metode shikon pas cdo levizje, a ka fitues
+        /// nese po, kthen mesazhin
+        /// </summary>
+        /// <returns>Mesazhi i kthyer, tregon 
+        /// fituesin (0 ose X)</returns>
+        private string ShikoAKaFitues()
+        {
+            //rreshti i pare (butonat 1,2,3)
+            if (AJaneTeNjejte(button1, button2, button3))
+                return $"Fitues eshte:{button1.Text}";
+
+            //rreshti i dyte (butonat 4,5,6)
+            if (AJaneTeNjejte(button4, button5, button6))
+                return $"Fitues eshte:{button4.Text}";
+
+            //rreshti i trete (butonat 7,8,9)
+            if (AJaneTeNjejte(button7, button8, button9))
+                return $"Fitues eshte:{button7.Text}";
+
+            //shtylla e pare (butonat 1,4,7)
+            if (AJaneTeNjejte(button1, button4, button7))
+                return $"Fitues eshte:{button1.Text}";
+
+            //shtylla e dyte (butonat 2,5,8)
+            if (AJaneTeNjejte(button2, button5, button8))
+                return $"Fitues eshte:{button2.Text}";
+
+            //shtylla e trete (butonat 3,6,9)
+            if (AJaneTeNjejte(button3, button6, button9))
+                return $"Fitues eshte:{button3.Text}";
+
+            //diagonalja e pare (butonat 1,5,9)
+            if (AJaneTeNjejte(button1, button5, button9))
+                return $"Fitues eshte:{button1.Text}";
+
+            //diagonalja e dyte (butonat 3,5,7)
+            if (AJaneTeNjejte(button3, button5, button7))
+                return $"Fitues eshte:{button3.Text}";
+
+            if (levizje == 9)
+                return "Loja barazim";
+
+            return "";
+        }
+
+        /// <summary>
+        /// Kjo metode kthen true nese 3 
+        /// butonat a,b dhe c jane X (ose 0) njekohesisht
+        /// </summary>
+        /// <param name="a">Butoni i pare</param>
+        /// <param name="b">Butoni i dyte</param>
+        /// <param name="c">Butoni i trete</param>
+        /// <returns>True nese 3 butonat jane X (ose 0)</returns>
+        private bool AJaneTeNjejte(Button a, Button b, Button c)
+        {
+            if (a.Text == b.Text && b.Text == c.Text && a.Text != "")
+                return true;
+
+            return false;
+        }
+
+        private void checkKunderKompj_CheckedChanged(object sender, EventArgs e)
+        {
+            // per te mos lejuar qe te selektohen te dy checkbox-at
+            if (checkKunderKompj.Checked && checkKunderKompjX.Checked)
+                checkKunderKompjX.Checked = false;
+        }
+
+        private void checkKunderKompjX_CheckedChanged(object sender, EventArgs e)
+        {
+            // per te mos lejuar qe te selektohen te dy checkbox-at
+            if (checkKunderKompjX.Checked && checkKunderKompj.Checked)
+                checkKunderKompj.Checked = false;
+        }
+    }
+}
